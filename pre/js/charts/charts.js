49fa28794830762bd66cebe685386afc6dbda5e1
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-25 12:10:20 +0200

    Primer desarrollo serio completado

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -1,23 +1,30 @@
 //Desarrollo de las visualizaciones
 import * as d3 from 'd3';
-//import { numberWithCommas2 } from './helpers';
-//import { getInTooltip, getOutTooltip, positionTooltip } from './modules/tooltip';
+import { numberWithCommas3 } from '../helpers';
+import { getInTooltip, getOutTooltip, positionTooltip } from '../modules/tooltip';
 import { setChartHeight } from '../modules/height';
 import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
 import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
 //Colores fijos
-const COLOR_PRIMARY_1 = '#F8B05C', 
-COLOR_PRIMARY_2 = '#E37A42',
+const COLOR_PRIMARY_1 = '#F8B05C',
 COLOR_COMP_1 = '#528FAD', 
 COLOR_COMP_2 = '#AADCE0',
-COLOR_GREY_1 = '#D6D6D6', 
-COLOR_GREY_2 = '#A3A3A3',
-COLOR_ANAG__PRIM_1 = '#BA9D5F', 
+COLOR_ANAG_PRIM_1 = '#BA9D5F', 
 COLOR_ANAG_PRIM_2 = '#9E6C51',
-COLOR_ANAG_PRIM_3 = '#9E3515',
 COLOR_ANAG_COMP_1 = '#1C5A5E';
+let tooltip = d3.select('#tooltip');
+
+//Diccionario
+let dictionary = {
+    0: '16-24',
+    1: '25-34',
+    2: '35-44',
+    3: '45-54',
+    4: '55-64',
+    5: '65+'
+};
 
 export function initChart(iframe) {
     //Lectura de datos
@@ -41,9 +48,13 @@ export function initChart(iframe) {
         let x = d3.scaleBand()
             .domain(d3.map(data, function(d){ return d.periodo; }).keys())
             .range([0, width])
-            .padding([0.2]);
+            .padding(0.2);
 
-        let xAxis = d3.axisBottom(x);
+        let xAxis = function(svg) {
+            svg.call(d3.axisBottom(x));
+            svg.call(function(g){g.selectAll('.tick line').remove()});
+            svg.call(function(g){g.selectAll('.domain').remove()});
+        }
         
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
@@ -53,13 +64,29 @@ export function initChart(iframe) {
             .domain([0, 100])
             .range([height, 0]);
 
+        let yAxis = function(svg) {
+            svg.call(d3.axisLeft(y).ticks(5).tickFormat(function(d,i) { return numberWithCommas3(d); }));
+            svg.call(function(g) {
+                g.call(function(g){
+                    g.selectAll('.tick line')
+                        .attr('class', function(d,i) {
+                            if (d == 0) {
+                                return 'line-special';
+                            }
+                        })
+                        .attr('x1', '0%')
+                        .attr('x2', `${width}`)
+                });
+            });
+        }
+
         svg.append("g")
             .attr("class", "yaxis")
-            .call(d3.axisLeft(y));
+            .call(yAxis);
 
         let color = d3.scaleOrdinal()
             .domain(gruposEdad)
-            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_GREY_1, COLOR_OTHER_1, COLOR_OTHER_2]);
+            .range([COLOR_PRIMARY_1, COLOR_ANAG_PRIM_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_ANAG_COMP_1, COLOR_ANAG_PRIM_2]);
 
         let stackedDataEdad = d3.stack()
             .keys(gruposEdad)
@@ -73,29 +100,66 @@ export function initChart(iframe) {
                 .enter()
                 .append("g")
                 .attr("fill", function(d) { return color(d.key); })
+                .attr("class", function(d,i) {
+                    return 'serie serie-' + i;
+                })
                 .selectAll("rect")
                 .data(function(d) { return d; })
                 .enter()
                 .append("rect")
-                    .attr('class','prueba')
-                    .attr("x", function(d) { return x(d.data.periodo) + x.bandwidth() / 4; })
-                    .attr("y", function(d) { return y(0); })
-                    .attr("height", function(d) { return 0; })
-                    .attr("width",x.bandwidth() / 2)
-                    .transition()
-                    .duration(2500)
-                    .attr("y", function(d) { return y(d[1]); })
-                    .attr("height", function(d) { return y(d[0]) - y(d[1]); });
+                .attr('class','rect')
+                .attr("x", function(d) { return x(d.data.periodo) + x.bandwidth() / 4; })
+                .attr("y", function(d) { return y(0); })
+                .attr("height", function(d) { return 0; })
+                .attr("width",x.bandwidth() / 2)
+                .on('mouseover', function(d,i,e) {
+                    //Opacidad en barras
+                    let bars = svg.selectAll('.rect');
+                    let parentElem = svg.select(`.${this.parentNode.classList.value.split(' ')[1]}`);
+                    let childElems = parentElem.selectAll('.rect');            
+            
+                    bars.each(function() {
+                        this.style.opacity = '0.4';
+                    });
+                    childElems.each(function() {
+                        this.style.opacity = '1';
+                    });
+
+                    //Tooltip
+                    let currentType = this.parentNode.classList.value.split(' ')[1];
+                    let html = '<p class="chart__tooltip--title">' + dictionary[currentType.split('-')[1]] + ' (' + d.data.periodo + ')</p>' + 
+                            '<p class="chart__tooltip--text">Este grupo de edad representa un <b>' + numberWithCommas3(d.data[dictionary[currentType.split('-')[1]]]) + '%</b> de la población total ocupada para este año</p>';
+                    
+                    tooltip.html(html);
+
+                    //Tooltip
+                    positionTooltip(window.event, tooltip);
+                    getInTooltip(tooltip);
+                })
+                .on('mouseout', function(d,i,e) {
+                    //Opacidad
+                    let bars = svg.selectAll('.rect');
+                    bars.each(function() {
+                        this.style.opacity = '1';
+                    });
+
+                    //Quitamos el tooltip
+                    getOutTooltip(tooltip);
+                })
+                .transition()
+                .duration(2000)
+                .attr("y", function(d) { return y(d[1]); })
+                .attr("height", function(d) { return y(d[0]) - y(d[1]); });
         }
 
         function animateChart() {
-            svg.selectAll('.prueba')
-                .attr("x", function(d) { return x(d.data.periodo); })
+            svg.selectAll('.rect')
+                .attr("x", function(d) { return x(d.data.periodo) + x.bandwidth() / 4; })
                 .attr("y", function(d) { return y(0); })
                 .attr("height", function(d) { return 0; })
-                .attr("width", x.bandwidth())
+                .attr("width", x.bandwidth() / 2)
                 .transition()
-                .duration(2500)
+                .duration(2000)
                 .attr("y", function(d) { return y(d[1]); })
                 .attr("height", function(d) { return y(d[0]) - y(d[1]); });
         }
